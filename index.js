4dd4c733eb9364e169b0841d9a71d7b43042a74d
HarshithsaiPondrati <138685515+HarshithsaiPondrati@users.noreply.github.com>
2023-11-21 18:19:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
-// index.js
 
-// Function to handle some functionality when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-  // Select an element with the id "app" from the DOM
-  const appElement = document.getElementById('app');
+// Function to validate the date of birth
+function validateDateOfBirth() {
+    var dobInput = document.getElementById("dob");
+    var dobValue = new Date(dobInput.value);
+    var currentDate = new Date();
+    var minDate = new Date(currentDate.getFullYear() - 55, currentDate.getMonth(), currentDate.getDate());
+    var maxDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
 
-  // Create a new paragraph element
-  const paragraphElement = document.createElement('p');
+    if (dobValue < minDate || dobValue > maxDate) {
+        alert("Date of birth must be between 18 and 55 years.");
+        dobInput.value = ""; // Clear the invalid date
+    } else {
+        alert("Thank you for providing a valid date of birth.");
+    }
+}
 
-  // Set the text content of the paragraph
-  paragraphElement.textContent = 'Hello, this content is added dynamically using JavaScript!';
+// Function to load saved data from web storage
+function loadSavedData() {
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    var tableBody = document.getElementById("tableBody");
 
-  // Append the paragraph element to the "app" element
-  appElement.appendChild(paragraphElement);
+    // Clear table body
+    tableBody.innerHTML = "";
 
-  // Add an event listener to the paragraph element
-  paragraphElement.addEventListener('click', function () {
-    alert('You clicked the paragraph!');
-  });
+    // Populate table with saved data
+    savedData.forEach(function (data) {
+        var row = tableBody.insertRow();
+        for (var key in data) {
+            var cell = row.insertCell();
+            cell.textContent = data[key];
+        }
+    });
+}
+
+// Function to handle form submission
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var dob = document.getElementById("dob").value;
+    var acceptTerms = document.getElementById("acceptTerms").checked;
+
+    var formData = { name: name, email: email, password: password, dob: dob, acceptTerms: acceptTerms };
+
+    // Save data to web storage
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    savedData.push(formData);
+    localStorage.setItem("registrationData", JSON.stringify(savedData));
+
+    // Load and display saved data
+    loadSavedData();
+    
+    // Validate date of birth and show an alert
+    validateDateOfBirth();
 });
+
+// Load saved data when the page loads
+window.onload = loadSavedData;
